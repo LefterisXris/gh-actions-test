092b97349aa42aca06dc6ac214251f547daf76e1
Eleftherios Chrysochoidis <lefterisxris@gmail.com>
2024-09-19 23:23:53 +0300

    init commit

---
+++ src/main/java/org/java/meetup/skg/ghactionstest/GhActionsTestApplication.java
@@ -0,0 +1,13 @@
+package org.java.meetup.skg.ghactionstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhActionsTestApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(GhActionsTestApplication.class, args);
+   }
+
+}
