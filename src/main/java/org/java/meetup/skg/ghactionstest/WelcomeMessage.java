13e58ce088132e7142d4f60e6176846bb114f77d
Eleftherios Chrysochoidis <lefterisxris@gmail.com>
2024-09-21 14:51:37 +0300

    fix test case

--- src/main/java/org/java/meetup/skg/ghactionstest/WelcomeMessage.java
+++ src/main/java/org/java/meetup/skg/ghactionstest/WelcomeMessage.java
@@ -1,16 +1,20 @@
 package org.java.meetup.skg.ghactionstest;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
 
 @Data
 public class WelcomeMessage {
    private final String message;
+   @EqualsAndHashCode.Exclude
    private final LocalDateTime dateTime;
 
    public WelcomeMessage(String message) {
       this.message = message;
       this.dateTime = LocalDateTime.now();
    }
+
+
 }
