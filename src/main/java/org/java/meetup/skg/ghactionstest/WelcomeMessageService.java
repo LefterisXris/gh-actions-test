daee84a7a096a68324062d8ad3a5b8edb4093d97
Eleftherios Chrysochoidis <lefterisxris@gmail.com>
2024-09-21 20:57:35 +0300

    new methods in Service

--- src/main/java/org/java/meetup/skg/ghactionstest/WelcomeMessageService.java
+++ src/main/java/org/java/meetup/skg/ghactionstest/WelcomeMessageService.java
@@ -5,4 +5,14 @@ public interface WelcomeMessageService {
    default WelcomeMessage getWelcomeMessage(String msg) {
       return new WelcomeMessage("Updated: " + msg);
    }
+
+   default WelcomeMessage getWelcomeMessage1(String msg) {
+      return new WelcomeMessage("Updated1: " + msg);
+   }
+
+   default WelcomeMessage getWelcomeMessage2(String msg) {
+      return new WelcomeMessage("Updated2: " + msg);
+   }
+
+
 }
